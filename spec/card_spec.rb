7b648aa7917e8c6ec3de4266b12a2afbdc8d61ca
Reenakoudi <kreena92@gmail.com>
2018-02-26 16:10:53 +0000

    Added a top_up method in card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,4 +5,13 @@ describe Card do
       expect(subject.balance).to eq (0)
     end
   end
+  describe '#top_up' do
+    it 'should respond to top_up with 1 argument' do
+    expect(subject).to respond_to(:top_up).with(1).argument
+   end
+    it 'should top up the card with the amount given' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+
+    end
+  end
 end
